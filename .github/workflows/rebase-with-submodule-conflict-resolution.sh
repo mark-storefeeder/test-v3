9321bfc747118694cb5ff5621bc771e698cd89b3
Mark Willmer <mark@storefeeder.com>
2025-06-04 13:35:47 +0100

    .

--- .github/workflows/rebase-with-submodule-conflict-resolution.sh
+++ .github/workflows/rebase-with-submodule-conflict-resolution.sh
@@ -1,53 +1,36 @@
 # Alias the input parameters to more descriptive names
 branch_to_rebase=$1
 base_branch=$2
-verbose_output=${3:-false}
+submodule_path=$3
 
-# Function to conditionally suppress output
-run_command() {
-  if [ "$verbose_output" = "true" ]; then
-    "$@"
-  else
-    "$@" &> /dev/null
-  fi
-}
+git restore . # Because we've given execute permissions to the script, we need to revert the change before checking out another branch
 
-run_command git restore . # Because we've given execute permissions to the script, we need to revert the change before checking out another branch
-
-if ! run_command git checkout $branch_to_rebase; then
+if ! git checkout $branch_to_rebase; then
   echo "ERROR: $branch_to_rebase could not be checked out."
   exit 1
 fi
 
-if ! run_command git submodule update --init --recursive; then
+if ! git submodule update --init --recursive; then
   echo "ERROR: $branch_to_rebase submodule update failed."
   exit 1
 fi
 
 handle_conflicts() {
   # Check whether there are any submodule conflicts
-  if git diff --name-only --diff-filter=U | grep -q "modules/test-components"; then
-    git add modules/test-components # Resolve the submodule conflict using the version in the branch that's being rebased - it'll get updated to the correct version later anyway
+  if git diff --name-only --diff-filter=U | grep -q "$submodule_path"; then
+    git add "$submodule_path" # Resolve the submodule conflict using the version in the branch that's being rebased
 
-    if ! run_command git -c advice.mergeConflict=false -c core.editor=true rebase --continue; then
+    if ! git -c advice.mergeConflict=false -c core.editor=true rebase --continue; then
       handle_conflicts # Recursively call handle_conflicts to handle multiple conflicts
     fi
   else
     echo "ERROR: $branch_to_rebase could not be rebased onto $base_branch because of non-submodule conflicts (or an unhandled error). Please rebase manually instead."
-    run_command git rebase --abort
+    git rebase --abort
     exit 1
   fi
 }
 
-# TEMP
-#cleanup() {
-#  run_command git checkout main
-#  run_command git submodule update --init --recursive
-#}
-#trap cleanup EXIT
-# TEMP END
-
-if run_command git -c advice.mergeConflict=false -c advice.submoduleMergeConflict=false rebase origin/$base_branch; then
+if git -c advice.mergeConflict=false -c advice.submoduleMergeConflict=false rebase origin/$base_branch; then
   # git push origin $branch_to_rebase --force-with-lease
   echo "SUCCESS: $branch_to_rebase was rebased onto $base_branch."
 else
