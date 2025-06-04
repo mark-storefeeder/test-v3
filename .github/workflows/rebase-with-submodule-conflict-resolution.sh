c65fbcb45585fc432dcb4458d320a79370d0b4e7
Mark Willmer <mark@storefeeder.com>
2025-06-04 16:31:16 +0100

    .

--- .github/workflows/rebase-with-submodule-conflict-resolution.sh
+++ .github/workflows/rebase-with-submodule-conflict-resolution.sh
@@ -3,8 +3,6 @@ branch_to_rebase=$1
 base_branch=$2
 submodule_path=$3
 
-current_branch=$(git branch --show-current)
-
 cleanup() {
   # Cleanup after ourselves in case subsequent scripts need to run
   git checkout $current_branch
@@ -20,37 +18,39 @@ handle_conflicts() {
       handle_conflicts # Recursively call handle_conflicts to handle multiple conflicts
     fi
   else
-    echo "ERROR: $branch_to_rebase could not be rebased onto $base_branch because of non-submodule conflicts. Please rebase manually instead."
+    echo "::error::Could not rebase $branch_to_rebase onto $base_branch because of non-submodule conflicts. Please rebase manually instead."
     git rebase --abort
     exit 1
   fi
 }
+
+current_branch=$(git branch --show-current)
 
 trap cleanup EXIT
 
 git restore . # Because we've given execute permissions to the script, we need to revert the change before checking out another branch
 
 if ! git checkout $branch_to_rebase; then
-  echo "ERROR: $branch_to_rebase could not be checked out."
+  echo "::error::Could not checkout $branch_to_rebase."
   exit 1
 fi
 
 if ! git submodule update --init --recursive; then
-  echo "ERROR: $branch_to_rebase submodule update failed."
+  echo "::error::Submodule update failed for $branch_to_rebase."
   exit 1
 fi
 
 if git -c advice.mergeConflict=false -c advice.submoduleMergeConflict=false rebase origin/$base_branch; then
   # git push origin $branch_to_rebase --force-with-lease
-  echo "SUCCESS: $branch_to_rebase was rebased onto $base_branch."
+  echo "::notice::Successfully rebased $branch_to_rebase onto $base_branch."
 else
   # Check if we're in a rebase state (which indicates conflicts)
   if [ -d ".git/rebase-merge" ] || [ -d ".git/rebase-apply" ]; then
     handle_conflicts
     # git push origin $branch_to_rebase --force-with-lease
-    echo "SUCCESS: $branch_to_rebase was rebased onto $base_branch with submodule conflict resolution."
+    echo "::notice::Successfully rebased $branch_to_rebase onto $base_branch with submodule conflict resolution."
   else
-    echo "ERROR: $branch_to_rebase could not be rebased onto $base_branch due to an unhandled error."
+    echo "::error::Could not rebase $branch_to_rebase onto $base_branch due to an unhandled error."
     exit 1
   fi
 fi
