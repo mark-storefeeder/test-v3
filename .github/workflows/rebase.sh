f07415730cfc9fc82bf085f3b9f0ed8533b19ab7
Mark Willmer <mark@storefeeder.com>
2025-06-04 13:10:49 +0100

    .

--- .github/workflows/rebase.sh
+++ .github/workflows/rebase.sh
@@ -12,7 +12,7 @@ run_command() {
   fi
 }
 
-run_command git restore . # Because we've given execute permissions to the script, we need to revert the change before checking out another branch
+#run_command git restore . # Because we've given execute permissions to the script, we need to revert the change before checking out another branch
 run_command git checkout $branch_to_rebase
 run_command git submodule update --init --recursive
 
