8e075b95ac79cf73ff4402e9f22da3adbc35ee90
Mark Willmer <mark@storefeeder.com>
2025-06-04 12:53:03 +0100

    .

--- .github/workflows/rebase.sh
+++ .github/workflows/rebase.sh
@@ -1,36 +1,44 @@
-# Update the rebase-branch.yml to run this script rather than inline scripts.
-
 # Alias the input parameters to more descriptive names
 branch_to_rebase=$1
 base_branch=$2
+verbose_output=${3:-false}
 
-git checkout $branch_to_rebase &> /dev/null
-git submodule update --init --recursive > /dev/null
+# Function to conditionally suppress output
+run_command() {
+  if [ "$verbose_output" = "true" ]; then
+    "$@"
+  else
+    "$@" &> /dev/null
+  fi
+}
+
+run_command git checkout $branch_to_rebase
+run_command git submodule update --init --recursive
 
 handle_conflicts() {
   # Check whether there are any submodule conflicts
   if git diff --name-only | grep -q "modules/test-components"; then
     git add modules/test-components # Resolve the submodule conflict using the version in the branch that's being rebased - it'll get updated to the correct version later anyway
 
-    if ! git -c advice.mergeConflict=false -c core.editor=true rebase --continue &> /dev/null; then
+    if ! run_command git -c advice.mergeConflict=false -c core.editor=true rebase --continue; then
       handle_conflicts # Recursively call handle_conflicts to handle multiple conflicts
     fi
   else
     echo "ERROR: $branch_to_rebase could not be rebased onto $base_branch because of non-submodule conflicts (or an unhandled error)."
-    git rebase --abort
+    run_command git rebase --abort
     exit 1
   fi
 }
 
 # TEMP
 #cleanup() {
-#  git checkout main &> /dev/null
-#  git submodule update --init --recursive > /dev/null
+#  run_command git checkout main
+#  run_command git submodule update --init --recursive
 #}
 #trap cleanup EXIT
 # TEMP END
 
-if git -c advice.mergeConflict=false -c advice.submoduleMergeConflict=false rebase origin/$base_branch &> /dev/null; then
+if run_command git -c advice.mergeConflict=false -c advice.submoduleMergeConflict=false rebase origin/$base_branch; then
   # git push origin $branch_to_rebase --force-with-lease
   echo "SUCCESS: $branch_to_rebase was rebased onto $base_branch."
 else
