edb21f1004b8bc29626053fd120c4d0c380d9000
Mark Willmer <mark@storefeeder.com>
2025-06-06 11:52:56 +0100

    .

---
+++ .github/workflows/scripts/get-pull-request-number.sh
@@ -0,0 +1,12 @@
+# Alias the input parameters to more descriptive names
+branch=$1
+
+pr_number=$(gh pr list --search "head:$branch" --json number --jq '.[0].number')
+
+if [ -z "$pr_number" ]; then
+  echo "::error::An open pull request does not exist for branch $branch"
+  exit 1
+fi
+
+echo "pr_number=$pr_number" >> $GITHUB_OUTPUT
+echo "::notice::Found pull request #$pr_number for branch $branch"
